ccf66f824bca341fdb107cd437abd397d39a32c9
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-09-02 14:50:06 +0300

    Navbar created

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,75 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <div className='relative w-28 h-14'>
-        <Image src="/Reddit-Logo.png" layout='fill' objectFit='contain' />
+    <div className=" px-4 flex flex-row items-center sticky top-0 z-50">
+      {/* Logo*/}
+      <div className="relative w-28 h-14 flex-shrink-0 cursor-pointer">
+        <Image src="/Reddit-Logo.png" layout="fill" objectFit="contain" />
+      </div>
+
+      {/* Home Btn*/}
+      <div className="flex items-center mx-7 xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="ml-2 hidden flex-1 lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+      {/* SearchBar*/}
+      <form className="flex flex-1 items-center space-x-2 border border-gray-200 rounded px-3 py-1 bg-gray-100">
+        <SearchIcon className="w-6 h-6 text-gray-400" />
+        <input
+          className="bg-transparent flex-1 outline-none"
+          placeholder="Search in Reddit "
+        />
+        <button type="submit" hidden />
+      </form>
+      {/* DEsktop Icons*/}
+      <div className="hidden lg:inline-flex space-x-2 text-gray-500 items-center mx-5">
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
+      </div>
+
+      {/*  MenuIcon   */}
+      <div className="lg:hidden ml-5 flex items-center">
+        <MenuIcon className="icon" />
+      </div>
+      {/*  Login/ LogOut Btn   */}
+
+      <div className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
+        <div className="relative h-5 w-5 flex-shrink-0">
+          <Image
+            src={"/Btn-Logo.png"}
+            objectFit="contain"
+            layout="fill"
+            alt=""
+          />
+        </div>
+        <p className="text-gray-600">Sign In</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 
+
 export default function Home() {
   return (
     <div>
